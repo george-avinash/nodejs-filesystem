226d3565db475a9ad074ffb3a6e5d028132f7ade
J George Avinash <145418454+george-avinash@users.noreply.github.com>
2024-01-29 01:46:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const app = express();
 const port = 3000;
-const filesDir = path.join("F:/Ms Visual/REAL/REACT/time", "files");
+const filesDir = path.join("project/build", "files");
 
 // Create 'files' directory if it doesn't exist
 if (!fs.existsSync(filesDir)) {
